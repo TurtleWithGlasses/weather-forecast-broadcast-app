0b72e778819abb09d8496564373968b018e2fc48
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2024-08-23 17:06:06 +0300

    Change the height of frame 1 & 2, add pady to frame 3 and change sticky to nsew

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ root.geometry("800x600")
 
 
 # frame for date, time, location, 7-dat forecast
-frame1 = tk.Frame(root, width=600, height=300, relief="ridge", bd=2, background="white")
+frame1 = tk.Frame(root, width=600, height=200, relief="ridge", bd=2, background="white")
 frame1.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
 # frame for 24-hour temperature bars
-frame2 = tk.Frame(root, width=600, height=150, relief="ridge", bd=2, background="white")
-frame2.grid(row=1, column=0, padx=10, pady=10, sticky="nw")
+frame2 = tk.Frame(root, width=600, height=300, relief="ridge", bd=2, background="white")
+frame2.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
 # frame for news headlines
 frame3 = tk.Frame(root, width=250, height=600, relief="ridge", bd=2, background="white")
-frame3.grid(row=0, column=1, rowspan=2, padx=10, sticky="nw")
+frame3.grid(row=0, column=1, rowspan=2, padx=10, pady=10, sticky="nsew")
 
 # date, time & location labels
 date_label = tk.Label(frame1, text=datetime.now().strftime("%A, %d, %B, %Y"), font=("Helvetica", 14))
